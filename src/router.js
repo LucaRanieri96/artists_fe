6dcbf890c92359083220584d685fc9f9cd5ad5fc
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-06-19 16:33:12 +0200

    Add header + footer components

--- src/router.js
+++ src/router.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "./components/Home.vue";
+import Home from "./view/Home.vue";
 
 const router = createRouter({
   history:createWebHistory(),
